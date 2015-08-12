07fd7e3d8b4216b7cdd2e4c820a0d2507a31b09e
Vincent Dutat <vdutat@nuxeo.com>
2015-08-12 16:51:28 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# nuxeo-redirection-after-login
+===
+
+## About this module
+
+This module provides an override of the Seam bean `startupHelper` which redirects the user after login.
+
+## Why this module
+
+Redirection after login can be configured by setting the configuration variable `CustomStartupHelper.viewOrPath` to a view ID or a path to a document.
+
+ex:
+
+- redirection to the user's home dashboard
+
+        CustomStartupHelper.viewOrPath=view_home
+
+    Other possible values: `view_servers`, `view_domains`
+        
+- redirection to thw workspaces' root of the default domain
+
+        CustomStartupHelper.viewOrPath=/defaut-domain/workspaces
+        
+## Building
+
+        mvn clean install
+
+## Using
+
+All you have to do is:
+
+ - copy the bundle in `nxserver/plugins` or `nxserver/bundles`
+ - restart the server
---
+++ src/main/java/org/nuxeo/ecm/custom/CustomStartupHelper.java
@@ -0,0 +1,59 @@
+package org.nuxeo.ecm.custom;
+
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
+import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.webapp.dashboard.DashboardNavigationHelper;
+import org.nuxeo.ecm.webapp.helpers.StartupHelper;
+import org.nuxeo.runtime.api.Framework;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Install;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Name("startupHelper")
+@Scope(org.jboss.seam.ScopeType.SESSION)
+@Install(precedence=Install.DEPLOYMENT)
+public class CustomStartupHelper extends StartupHelper {
+
+    protected static final String HOME_VIEW = "view_home";
+
+//    @In(create = true)
+//    protected DashboardNavigationHelper dashboardNavigationHelper;
+
+    @In(create = true)
+    protected NuxeoPrincipal currentUser;
+
+    private static final long serialVersionUID = 1L;
+
+    private static final Log LOGGER = LogFactory.getLog(CustomStartupHelper.class);
+
+    @Override
+    public String initDomainAndFindStartupPage(String domainTitle, String viewId) {
+        String view = super.initDomainAndFindStartupPage(domainTitle, viewId);
+        String viewOrPath = Framework.getProperty("CustomStartupHelper.viewOrPath");
+        if (viewOrPath != null) {
+            String defaultErrMessage = "Unable to redirect to " + viewOrPath + ", redirecting to default page ...";
+            if (viewOrPath.startsWith("/")) {
+                PathRef ref = new PathRef(viewOrPath);
+                try {
+                    if (documentManager.exists(ref)) {
+                        view = navigationContext.navigateToRef(ref);
+                    } else {
+                        LOGGER.error(defaultErrMessage + " 1");
+                    }
+                } catch (ClientException e) {
+                    LOGGER.error(defaultErrMessage + " 2");
+                }
+            } else {
+                if (HOME_VIEW.equals(viewOrPath)) {
+                    return dashboardNavigationHelper.navigateToDashboard();
+                }
+            }
+        }
+        return view;
+    }
+
+}
